76b2a9e6abc85b9a57e2491885e7e6b14bb5a944
zakve <zaklas.martin@gmail.com>
2021-01-28 20:57:44 +0100

    Add mapbox geocode request

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const request = require('postman-request')
 const dotenv = require('dotenv').config()
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=49.19,16.57`
+const weatherURL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=49.19,16.57`
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_KEY}`
 
-request(url, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({url: weatherURL, json: true}, (error, response) => {
+    console.log(response.body.current)
+})
+
+request({url: geocodeURL, json: true}, (error, response) => {
+    const latitude = response.body?.features[0]?.center[0]
+    const longitude = response.body?.features[0]?.center[1]
+    console.log(latitude, longitude)
 })
