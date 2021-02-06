0a884f61b1a74fff69fe2d4edaa711560aeedea1
zakve <zaklas.martin@gmail.com>
2021-02-06 11:40:11 +0100

    Refactor destructuring objects

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const address = process.argv[2]
 if (!address) {
     console.log('Please provide an address')
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
         if (error) {
             return console.log('Error', error)
         }
     
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log('Error', error)
             }
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
     })
--- utils/forecast.js
+++ utils/forecast.js
@@ -4,14 +4,14 @@ const dotenv = require('dotenv').config()
 const forecast = (lat, lon, callback) => {
     const weatherURL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=${lon},${lat}`
 
-    request({url: weatherURL, json: true}, (error, response) => {
+    request({url: weatherURL, json: true}, (error, {body} = {}) => {
         if (error) {
             callback('Unable to connect to weather service', undefined)
-        } else if (response.body?.error) {
+        } else if (body?.error) {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, {
-                current: response.body.current
+                current: body?.current
             })
         }
     })
