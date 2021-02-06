1e7c287e0c9daae6f5a7df7d1e9835236dcb6ae4
zakve <zaklas.martin@gmail.com>
2021-02-06 11:21:42 +0100

    Add argument from console

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Brno', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const address = process.argv[2]
 
-forecast(14.42139, 50.08861, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log('Error', error)
+        }
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log('Error', error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
