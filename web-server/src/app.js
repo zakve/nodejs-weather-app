e367ecc119f67da8f39d9210754e62fa5936a116
zakve <zaklas.martin@gmail.com>
2021-03-06 21:24:41 +0100

    Add publicDirecotryPath

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,10 @@
+const path = require("path")
 const express = require("express")
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 app.get("", (req, res) => {
     res.send("Hey")
